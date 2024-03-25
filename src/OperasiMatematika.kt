36f447d63c768f6483a26b93186d950af75d19ff
latif <latifirfansyah@gmail.com>
2024-03-25 14:19:53 +0700

    add math operation and condition

---
+++ src/OperasiMatematika.kt
@@ -0,0 +1,22 @@
+fun main(){
+
+    // perkalian
+    val result1 = 5 * 5
+    println(result1)
+
+    // pembagian
+    val result2 = 10 / 2
+    println(result2)
+
+    // pengurangan
+    val result3 = 5 - 2
+    println(result3)
+
+    // penjumlahan
+    val result4 = 4 + 8
+    println(result4)
+
+    // modulus
+    val result5 = 13 % 4
+    println(result5)
+}
---
+++ src/Pengkondisian.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val a = 9
+    if (a % 2 == 0){
+        println("a itu genap")
+    } else if (a % 2 != 0){
+        println("a itu ganjil")
+    }
+}
