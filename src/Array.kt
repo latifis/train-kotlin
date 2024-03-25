e95c33d882db725514301191795863a7c9f646df
latif <latifirfansyah@gmail.com>
2024-03-25 14:21:30 +0700

    add loop

--- src/Array.kt
+++ src/Array.kt
@@ -4,4 +4,9 @@ fun main() {
     val member: Array<Any> = arrayOf("latif", "irfan", "sya", 1)
     println(member.map { it })
     println(member.contentToString())
+
+    // Loop
+    for (name in member) {
+        println(name)
+    }
 }
